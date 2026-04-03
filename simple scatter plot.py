f640f46834d7dde1293e47659101a8ab5601047d
imhim395 <yuvaangulati7689@gmail.com>
2026-04-03 15:01:24 -0400

    commit and push

---
+++ simple scatter plot.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+year = [1950,1970,1990,2010]
+population = [2.519, 3.692, 5.263, 6.972]
+plt.scatter(year, population)#makes ts a scatter plot (plots individual points)
+plt.show()
